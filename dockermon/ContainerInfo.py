fca1ca7c6ad2bbfde8a9e145afbf4f04fe97dbbe
SaraQian <abigail830@163.com>
2019-01-10 22:15:31 +0800

    update portInfo

--- dockermon/ContainerInfo.py
+++ dockermon/ContainerInfo.py
@@ -1,16 +1,15 @@
 from dockermon.PortInfo import PortInfo
-
+import json
 
 class ContainerInfo:
-    names = []
+    names = 'Nil'
     status = 'Nil'
     ports = []
 
     def __init__(self, container):
 
         if 'Names' in container.keys():
-            for name in container['Names']:
-                self.names.append(name)
+            self.names = container['Names']
 
         if 'State' in container.keys():
             self.state = container['State']
@@ -19,6 +18,9 @@ class ContainerInfo:
             for port in container['Ports']:
                 port_info = PortInfo(port)
                 self.ports.append(port_info)
+
+        ports_json = json.dumps([port.__dict__ for port in self.ports])
+        print("{}".format(ports_json))
 
     def __str__(self):
         result = "Docker {} is {} with Ports {} ".format(self.name, self.state, self.ports)
