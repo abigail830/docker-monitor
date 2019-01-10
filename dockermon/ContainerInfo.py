ca766b686ec1ea7dc3de4b84a97cc1e12c02eb72
SaraQian <abigail830@163.com>
2019-01-10 20:48:38 +0800

    update portInfo

--- dockermon/ContainerInfo.py
+++ dockermon/ContainerInfo.py
@@ -5,11 +5,10 @@ class ContainerInfo:
     def __init__(self, name, ports, status):
         self.name = name
         self.status = status
-        self.ports = ports
+        self.ports = []
         for port in ports:
             port_info = PortInfo(port['PrivatePort'])
             self.ports.append(port_info)
-            print("{}".format(port))
 
     def __str__(self):
         result = "Docker {} with Ports {} is {}".format(self.name, self.ports, self.status)
