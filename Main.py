dcf65042e478c0633e652c84c5237b8fbe7b0419
SaraQian <abigail830@163.com>
2019-01-10 23:09:24 +0800

    update port

--- Main.py
+++ Main.py
@@ -2,6 +2,6 @@ from dockermon.SimpleHTTPRequestHandler import SimpleHTTPRequestHandler
 from http.server import HTTPServer
 
 if __name__ == '__main__':
-    httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+    httpd = HTTPServer(('localhost', 8099), SimpleHTTPRequestHandler)
     httpd.serve_forever()
 
